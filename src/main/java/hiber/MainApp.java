989c25f386b346c46134cd3e62ff1d104b4e81b5
MrRich-prog <rasskazov20@list.ru>
2025-03-19 13:17:47 +0300

    second commit

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -17,22 +17,18 @@ public class MainApp {
         userService.cleanAllTable();
 
         Car car1 = new Car(1, "car1");
-        userService.add(car1);
         User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
         userService.add(user1);
 
         Car car2 = new Car(2, "car2");
-        userService.add(car2);
         User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
         userService.add(user2);
 
         Car car3 = new Car(3, "car3");
-        userService.add(car3);
         User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
         userService.add(user3);
 
         Car car4 = new Car(4, "car4");
-        userService.add(car4);
         User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
         userService.add(user4);
 
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,5 @@
 package hiber.dao;
 
-import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +20,10 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    public void add(Car car) {
-        sessionFactory.getCurrentSession().save(car);
-    }
-
-    @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("select user from User user join fetch user.car");
         return query.getResultList();
     }
 
@@ -36,9 +31,8 @@ public class UserDaoImp implements UserDao {
     @SuppressWarnings("unchecked")
     public User findUserByCar(int series, String model) {
         TypedQuery<User> query = sessionFactory.getCurrentSession()
-                .createQuery("select user from User user where" +
-                        " user.car.model = :model and" +
-                        " user.car.series = :series", User.class);
+                .createQuery("select user from User user join fetch user.car where" +
+                        " user.car.model = :model and user.car.series = :series", User.class);
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList().isEmpty() ? null : query.getResultList().get(0);
